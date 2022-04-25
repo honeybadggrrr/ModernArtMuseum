714bab2a74fbaf7e7ea1c0f2c691106943ea7fc5
vladyslav.kostyria <vladyslav.kostyria@decsoft.com.pl>
2022-04-25 10:05:47 +0200

    Initial commit

---
+++ src/main/java/com/example/modernartmuseum/ModernArtMuseumApplication.java
@@ -0,0 +1,13 @@
+package com.example.modernartmuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernArtMuseumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernArtMuseumApplication.class, args);
+    }
+
+}
